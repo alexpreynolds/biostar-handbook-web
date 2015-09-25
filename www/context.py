cf2c030b9a3c3ed1da586a133ff8312e3eeb7581
Istvan Albert <istvan.albert@gmail.com>
2015-09-25 10:48:33 -0400

    refactored file names

--- www/context.py
+++ www/context.py
@@ -6,36 +6,28 @@ import os, csv, glob
 __this = os.path.dirname(__file__)
 os.chdir(__this)
 
-# Collect all chapter names.
-names = glob.glob("unit/???-*.html")
-names = sorted(names)
+CHAPTERS = [
+    "unix-intro.html",
+    "ncbi-entrez.html",
+    "short-read-archive.html",
+    "fastq-quality-control.html",
+    "unix-shell-scripts.html",
 
-ICON_PATT = '<span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span> %d'
+    # Keep this last chapter.
+    "software-installation-guide.html",
+]
 
-CHAPTERS = []
-for idx, fname in zip(count(1), names):
-    __, name = os.path.split(fname)
-    CHAPTERS.append(
-        (fname, ICON_PATT % idx)
-    )
-
-MINI_NAVBAR = [
+NAVBAR = [
     ("index.html", '<span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home'),
     ("contribute.html", '<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add'),
     ("about.html", '<span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> About'),
 ]
 
-# This is the navigation bar on top.
-# NAVBAR = MINI_NAVBAR[:1] + CHAPTERS + MINI_NAVBAR[1:]
 
-# Too many chapters. Need to rethink what will be shown as shortcuts.
-NAVBAR = MINI_NAVBAR
+# Example numbers. Used for testing.
+NUMBERS = range(1, 4)
 
 
-# Example numbers.
-numbers = range(1, 4)
-
-
-# Example function call.
+# Example function call. Used for testing.
 def say_hello():
     return "Hello World!"
